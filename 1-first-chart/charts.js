2a3c9cb022064a70cd891730be039f9ed45ea8dc
JulianVoila <119368018+JulianVoila@users.noreply.github.com>
2022-11-30 11:43:25 -0500

    Lesson 1 - 4

--- 1-first-chart/charts.js
+++ 1-first-chart/charts.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3"
 
 async function drawLineChart() {
-  const data = await d3.json("./my_weather_data.json")
+  const data = await d3.json("my_weather_data.json")
   console.log(data)
 
   const yAccesor = d => d["temperatureMax"]
@@ -10,6 +10,25 @@ async function drawLineChart() {
   const parseDate = d3.parseDate("%Y-%m-%d")
   const xAccesor = d => parseDate(d["date"])
   console.log(xAccesor(data[0]))
+
+  let dms = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margins: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    }
+  }
+
+  dms.boundedWidth = dms.width - dms.margins.left - dms.margins.right
+  dms.boundedHeight = dms.width - dms.margins.top - dms.margins.bottom
+
+  const wrapper = d3.select("#wrapper")
+  console.log(wrapper)
+
+  const svg = wrapper.append("svg")
 }
 
 drawLineChart()
